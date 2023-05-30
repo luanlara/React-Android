0d61b8b5a73700c52ef0544c2d65debd1449cac4
Luan Lara de Moraes <luanlaramoraes@gmail.com>
2023-05-30 19:26:11 -0300

    Aplicativo react de pesquisas de universidades por país. Atividade de programação mobile

---
+++ Pesquisa-Universidades/components/Universidade.js
@@ -0,0 +1,9 @@
+import { TurboModuleRegistry } from "react-native/types";
+import { Text } from "react-native/types";
+
+export default function Universidade() {
+    return <Text>"Oi"</Text>
+}
+
+
+module.exports.Universidade = Universidade;
